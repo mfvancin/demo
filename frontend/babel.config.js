d97da62ed2a29100a1bd684e6c2a88e1544e6e1a
Manuele Vancin <mfvancin@gmail.com>
2025-06-26 17:06:56 +0100

    build changes

---
+++ frontend/babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@components': './app/components',
+            '@data': './app/data',
+            '@navigation': './app/navigation',
+            '@screens': './app/screens',
+            '@theme': './app/theme',
+            '@types': './app/types',
+            '@services': './app/services',
+          }
+        }
+      ]
+    ]
+  };
+}; 

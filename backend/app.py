7f7b4123a334c04419a45edfdff21aade178cfef
Manuele Vancin <mfvancin@gmail.com>
2025-06-30 12:55:16 +0100

    server

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -80,4 +81,5 @@ def get_doctor_patients(doctor_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True) 
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port) 
